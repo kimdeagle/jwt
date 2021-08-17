d5419c59e42ba1491614418b94c86aaf63936e2c
Juhyeok Kim <juhyeok.dev@gmail.com>
2021-08-17 18:00:07 +0900

    필터 등록 테스트

---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.cos.jwt.config;
+
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter3;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //http.addFilterBefore(new MyFilter1(), BasicAuthenticationFilter.class);
+        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션을 사용하지 않겠다.
+                .and()
+                .addFilter(corsFilter) //@CrossOrigin(인증X), 필터에 등록(인증O)
+                .formLogin().disable() //formLogin 사용 X
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+    }
+}
---
+++ src/main/java/com/cos/jwt/filter/MyFilter3.java
@@ -0,0 +1,36 @@
+package com.cos.jwt.filter;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyFilter3 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        //토큰 : cos 이걸 만들어줘야 한다. 언제? ID, PW 정상적으로 들어와서 로그인이 완료되면 토큰을 만들어주고 응답해준다.
+        //요청할 때마다 header에 Authorization에 value값으로 토큰을 가지고 온다.
+        //그 때, 토큰이 넘어오면 
+        if (StringUtils.equals(req.getMethod(), "POST")) {
+            System.out.println("POST 요청됨");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println("headerAuth : "+headerAuth);
+            System.out.println("필터3");
+
+            if (StringUtils.equals(headerAuth, "cos")) {
+                chain.doFilter(request, response);
+            } else {
+                PrintWriter writer = res.getWriter();
+                writer.println("인증안됨");
+                writer.close();
+            }
+        }
+    }
+}
